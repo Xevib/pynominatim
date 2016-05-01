8d516c985ce77b4cb83d958b28f5baf4a1756112
Xevi <xbarnada@gmail.com>
2016-05-01 10:14:42 +0200

    Rename module

--- pynominatim/__init__.py
+++ pynominatim/__init__.py
@@ -1,4 +1,4 @@
 """Nominatim API
 http://wiki.openstreetmap.org/wiki/Nominatim"""
 from __future__ import absolute_import
-from .nominatim import Nominatim, NominatimReverse, NominatimException
+from .pynominatim import Nominatim, NominatimReverse, NominatimException
